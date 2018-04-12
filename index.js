3e65312c9e8a28051032adf2c461bfadba5182a1
Ditto Harnando <harnando.ditto@gmail.com>
2018-04-12 15:55:06 +0000

    index.js edited online with Bitbucket

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const line = require('@line/bot-sdk');
 
 const client = new line.Client({
-  channelAccessToken: '<26D2SnIthQ4cAFOCOXYZYCIbKIircrAmcx3vzw5lgUgRRURoSuy4XNrCivct7HQ65XPEgCTsjWSlWzeVusIxbIdsKybwC1BkEiOlS711irAed3HyFhxkEdnL4Kie/2FUWMtIOiXlwqNQTkYLMTuZkAdB04t89/1O/w1cDnyilFU=>'
+  channelAccessToken: '26D2SnIthQ4cAFOCOXYZYCIbKIircrAmcx3vzw5lgUgRRURoSuy4XNrCivct7HQ65XPEgCTsjWSlWzeVusIxbIdsKybwC1BkEiOlS711irAed3HyFhxkEdnL4Kie/2FUWMtIOiXlwqNQTkYLMTuZkAdB04t89/1O/w1cDnyilFU='
 });
 
 const message = {
