667f6aca72f5ff864e03320696649e48d6a7b781
Ditto Harnando <harnando.ditto@gmail.com>
2018-04-12 01:29:21 +0000

    index.js edited nyobain sample script node.js

--- index.js
+++ index.js
@@ -62,12 +62,9 @@ app.listen(port, () => {
 });
 
 
-GREETING_KEYWORDS = ("hello", "hi", "greetings", "sup", "what's up",)
+var builder = require('botbuilder');
 
-GREETING_RESPONSES = ["'sup bro", "hey", "*nods*", "hey you get my snap?"]
-
-def check_for_greeting(sentence):
-    """If any of the words in the user's input was a greeting, return a greeting response"""
-    for word in sentence.words:
-        if word.lower() in GREETING_KEYWORDS:
-            return random.choice(GREETING_RESPONSES)
+var connector = new builder.ConsoleConnector().listen();
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send("You said: %s", session.message.text);
+});
