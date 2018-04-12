43469327450b687e48e90d938c338032a5309bcb
Ditto Harnando <harnando.ditto@gmail.com>
2018-04-12 15:52:15 +0000

    index.js edited online with Bitbucket

--- index.js
+++ index.js
@@ -1,67 +1,9 @@
-'use strict';
-
 const line = require('@line/bot-sdk');
-const express = require('express');
-const Config = require('./config.json')
-const rp = require('request-promise')
-const Humanize = require('humanize-plus')
 
-// create LINE SDK config from env variables
-const config = {
-  channelAccessToken: Config.accessToken,
-  channelSecret: Config.secretKey
-};
-
-// create LINE SDK client
-const client = new line.Client(config);
-
-const app = express();
-
-// register a webhook handler with middleware
-// about the middleware, please refer to doc
-app.post('/callback', line.middleware(config), (req, res) => {
-  Promise
-    .all(req.body.events.map(handleEvent))
-    .then((result) => res.json(result));
+const client = new line.Client({
+  channelAccessToken: '<26D2SnIthQ4cAFOCOXYZYCIbKIircrAmcx3vzw5lgUgRRURoSuy4XNrCivct7HQ65XPEgCTsjWSlWzeVusIxbIdsKybwC1BkEiOlS711irAed3HyFhxkEdnL4Kie/2FUWMtIOiXlwqNQTkYLMTuZkAdB04t89/1O/w1cDnyilFU=>'
 });
 
-// event handler
-async function handleEvent(event) {
-  if (event.type !== 'message' || event.message.type !== 'text') {
-    // ignore non-text-message event
-    return Promise.resolve(null);
-  }
-
-
-  var params = event.message.text.toUpperCase().split(' ');
-  var opts = { method: "GET",uri: Config.api + '/data/pricemulti?fsyms='+params[0]+'&tsyms=IDR,USD,EUR',resolveWithFullResponse: true}
-  var result = await rp(opts).then(result=> {
-    return result.body;
-  });
-  var text = JSON.parse(result)
-
-  var reply = `
-  ${params[0]} Now
-  IDR : Rp. ${Humanize.formatNumber(text[params[0]].IDR,0)}
-  USD : USD ${Humanize.formatNumber(text[params[0]].USD,0)}
-  EUR : EUR ${Humanize.formatNumber(text[params[0]].EUR,0)}
-  `
-
-
-  const echo = { type: 'text', text: reply };
-
-  // use reply API
-  return client.replyMessage(event.replyToken, echo);
-}
-
-
-// listen on port
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`listening on ${port}`);
-});
-
-// sample API
 const message = {
   type: 'text',
   text: 'Hello World!'
