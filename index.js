b40402577bb5fd415e1b38a099b7cfdd0dd28172
Ditto Harnando <harnando.ditto@gmail.com>
2018-04-12 16:22:33 +0000

    index.js edited online with Bitbucket

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-'use strict';
-
 const line = require('@line/bot-sdk');
-const express = require('express');
 const Config = require('./config.json')
-const rp = require('request-promise')
-const Humanize = require('humanize-plus')
 
 // create LINE SDK config from env variables
 const config = {
@@ -15,48 +10,16 @@ const config = {
 // create LINE SDK client
 const client = new line.Client(config);
 
-const app = express();
+//Convo Script
+const message = {
+  type: 'yolo',
+  text: 'Hello World!'
+};
 
-// register a webhook handler with middleware
-// about the middleware, please refer to doc
-app.post('/callback', line.middleware(config), (req, res) => {
-  Promise
-    .all(req.body.events.map(handleEvent))
-    .then((result) => res.json(result));
-});
-
-// event handler
-async function handleEvent(event) {
-  if (event.type !== 'message' || event.message.type !== 'text') {
-    // ignore non-text-message event
-    return Promise.resolve(null);
-  }
-
-
-  var params = event.message.text.toUpperCase().split(' ');
-  var opts = { method: "GET",uri: Config.api + '/data/pricemulti?fsyms='+params[0]+'&tsyms=IDR,USD,EUR',resolveWithFullResponse: true}
-  var result = await rp(opts).then(result=> {
-    return result.body;
+client.replyMessage('<replyToken>', message)
+  .then(() => {
+    ...
+  })
+  .catch((err) => {
+    // error handling
   });
-  var text = JSON.parse(result)
-
-  var reply = `
-  ${params[0]} Now
-  IDR : Rp. ${Humanize.formatNumber(text[params[0]].IDR,0)}
-  USD : USD ${Humanize.formatNumber(text[params[0]].USD,0)}
-  EUR : EUR ${Humanize.formatNumber(text[params[0]].EUR,0)}
-  `
-
-
-  const echo = { type: 'text', text: reply };
-
-  // use reply API
-  return client.replyMessage(event.replyToken, echo);
-}
-
-
-// listen on port
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`listening on ${port}`);
-});
